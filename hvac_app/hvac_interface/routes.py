2f6c50f4ba843a9cf124f653a13f1351efb16a53
Mohammed Elnomros <m.alnomrosi@hotmail.com>
2025-10-20 10:21:49 +0200

    add github LICENSE

--- hvac_app/hvac_interface/routes.py
+++ hvac_app/hvac_interface/routes.py
@@ -11,12 +11,11 @@ def hvac_get_tempratures():
     """Return information about the available data to be diagnostic inside HVAC CONTROL Application"""
 
     hvac_diag = 0
-    data = {}
     # Reading a YAML file
     with open(DiagLocations.HVAC_GEN_DATA_DIAG_LOC, 'r') as file:
         hvac_diag = yaml.safe_load(file) 
     
-    data_diag = hvac_diag["HvacControl"]
+    data_diag = hvac_diag[DiagNames.APP_NAME]
     yaml_output = yaml.dump(data_diag, default_flow_style=False)
 
     return yaml_output
