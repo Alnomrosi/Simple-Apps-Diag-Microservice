9c91c1c63cae88a06b0b4658876d709ce036af6d
Mohammed Elnomros <m.alnomrosi@hotmail.com>
2025-10-22 14:04:40 +0200

    creating data provider structure

--- hvac_app/data_model/model.py
+++
@@ -1,9 +0,0 @@
-
-
-# Temprature "Model"
-Temprature = {
-    "front_right": 0,
-    "front_left": 0,
-    "rear_right": 0,
-    "rear_left": 0
-}
--- hvac_app/hvac_interface/routes.py
+++ hvac_app/hvac_interface/routes.py
@@ -5,25 +5,24 @@ import os
 MODE = os.getenv("APP_MODE", "TESTING")  # or MODE = "DEPLOYMENT"
 
 if MODE == "DEPLOYMENT":
-    from hvac_app.data_model.model import Temprature
-    from hvac_app.hvac_interface.diag_information import DiagLocations, DiagNames
+    from hvac_app.hvac_interface.diag_path import DiagLocations, DiagNames
 if MODE == "TESTING":
-    from data_model.model import Temprature
-    from hvac_interface.diag_information import DiagLocations, DiagNames
+    from hvac_interface.diag_path import DiagLocations, DiagNames
 
 
 routes = Blueprint('routes', __name__)
 
-# app/data
+########
+# DATA #
+########
+# app-id/data
 @routes.route('/hvac/data', methods=['GET'])
 def hvac_get_tempratures():
     """Return information about the available data to be diagnostic inside HVAC CONTROL Application"""
-
     hvac_diag = 0
-    # Reading a YAML file
+    # Reading data/general_diag YAML file
     with open(DiagLocations.HVAC_GEN_DATA_DIAG_LOC, 'r') as file:
         hvac_diag = yaml.safe_load(file)
     
     data_diag = hvac_diag[DiagNames.APP_NAME]
-
     return data_diag
