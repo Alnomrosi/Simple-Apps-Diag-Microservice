08970d8eb13cdd9f0aa9db057fd27ba1c8c12d17
Mohammed Elnomros <m.alnomrosi@hotmail.com>
2025-10-24 12:00:22 +0200

    adding data_id endpoint

--- hvac_app/hvac_interface/routes.py
+++ hvac_app/hvac_interface/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 import os
+import requests
 
 MODE = os.getenv("APP_MODE", "TESTING")  # or MODE = "DEPLOYMENT"
 
@@ -19,8 +20,16 @@ data_provider = DataProvider()
 def get_datas_from_application(app_id:str):
     """Return information about the available data to be diagnostic inside HVAC CONTROL Application"""
     app_data = 0
-    
     # Reading data/general_diag YAML file
     app_data = data_provider.get_dids(app_id)
-    
     return app_data
+
+@routes.route("/<app_id>/data/<data_id>", methods=['GET'])
+def set_data_id_from_application(app_id:str,data_id:str):
+    try:
+        r = requests.get("http://127.0.0.1:6000/hvac/FrontLeftTemp", timeout=1.0)
+        r.raise_for_status()
+        return (r.text, r.headers.get('Content-Type', 'application/json'))
+    except requests.RequestException as e:
+        return jsonify({"error": "failed to reach hvac service", "details": str(e)}), 502
+    
